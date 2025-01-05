1bb449f3f9d6e8811953785a0f972f7adcfdd172
haitianlan <31479870+v2vv@users.noreply.github.com>
2025-01-05 19:32:27 +0800

    Update diy-part2.sh

--- build/lede/diy-part2.sh
+++ build/lede/diy-part2.sh
@@ -14,10 +14,10 @@
 # sed -i 's/192.168.1.1/192.168.1.59/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile
 
 # Modify hostname
 # sed -i 's/LEDE/OPENWRT/g' package/base-files/files/bin/config_generate
